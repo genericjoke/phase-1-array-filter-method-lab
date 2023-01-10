914aa6111413b348cb1c13ebb1c44d071620b50a
Nick Sollon <genericjoke@gmail.com>
2023-01-10 12:45:31 -0500

    completed finally

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+function findMatching(collection, name) {
+    let match = [];
+    for (const driver of collection) {
+      if ((driver).toLowerCase() == name.toLowerCase()) {
+        match.push(driver);
+      }
+    }
+    return match;
+  }
+  function fuzzyMatch(collection, lets){
+    let match = [];
+    for (const driver of collection) {
+      if ((driver.slice(0,lets.length).toLowerCase()).includes(lets.toLowerCase())) {
+        match.push(driver);
+      }
+    }
+    return match;
+  }
+
+  function matchName(collection, name){
+    let match = [];
+    for (const driver of collection) {
+      if ((driver.name.toLowerCase()).includes(name.toLowerCase())) {
+        match.push(driver);
+      }
+    }
+    return match;
+  }
